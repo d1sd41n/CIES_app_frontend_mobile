bd0c3779429cceca7416e97d8423652f2b180b01
Dan <ddd666.dt@gmail.com>
2020-05-30 14:28:24 -0500

    edit item update

--- src/screens/editItemScreen.js
+++ src/screens/editItemScreen.js
@@ -1,7 +1,6 @@
 import React, {useState, useContext, useEffect } from 'react';
 import { StyleSheet, TouchableOpacity, View, AsyncStorage, ActivityIndicator, Alert} from 'react-native';
-import { Text, Input, FormLabel, FormInput,} from 'react-native-elements';
-import { Ionicons } from '@expo/vector-icons';
+import { Text, Input, Button, FormInput,} from 'react-native-elements';
 import { NavigationEvents } from 'react-navigation';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
@@ -18,7 +17,8 @@ const ItemRegister = ({navigation}) => {
 
   console.log(state)
     return (
-      <View >
+      <KeyboardAwareScrollView extraScrollHeight={100} enableOnAndroid={true} 
+          keyboardShouldPersistTaps='handled'>
         <NavigationEvents 
           onWillBlur={onWillBlur}/>
       <Spacer>
@@ -29,32 +29,101 @@ const ItemRegister = ({navigation}) => {
         Por ahora solo se pueden cambiar los estados perdido/encontrado</Text>
       </Spacer>
       <Text style={styles.label}>
-        Nombre:
+        Propietario:
       </Text>
       <Input 
           
           inputStyle={styles.input}
-          placeholder='Color'
-          value="Hola"
-          onChangeText={(newColor) => setColor(newColor)}
+          placeholder=''
+          value={state.data.owner_name +
+                 " " + state.data.owner_last_name +
+                 " - " + state.data.owner_dni}
           autoCapitalize="none"
           autoCorrect={false}
           disabled={true}
           />
       <Text style={styles.label}>
-        Nombre:
+        Tipo de objeto:
       </Text>
       <Input 
           
           inputStyle={styles.input}
-          placeholder='Color'
-          value="Hola"
-          onChangeText={(newColor) => setColor(newColor)}
+          placeholder=''
+          value={state.data.type_item}
           autoCapitalize="none"
           autoCorrect={false}
           disabled={true}
           />
-      <Spacer />
+      <Text style={styles.label}>
+        Marca:
+      </Text>
+      <Input 
+          
+          inputStyle={styles.input}
+          placeholder=''
+          value={state.data.brand}
+          autoCapitalize="none"
+          autoCorrect={false}
+          disabled={true}
+      />
+      <Text style={styles.label}>
+        Estado:
+      </Text>
+      {state.data.lost  ? 
+          <Input 
+          inputStyle={styles.lostInput}
+          placeholder=''
+          value="Perdido"
+          disabled={true}
+          />
+      : 
+          <Input 
+          inputStyle={styles.notLostInput}
+          placeholder=''
+          value="Encontrado"
+          disabled={true}
+          />
+      }
+      <Text style={styles.label}>
+        Referencia:
+      </Text>
+      <Input 
+          
+          inputStyle={styles.input}
+          placeholder=''
+          value={state.data.reference}
+          autoCapitalize="none"
+          autoCorrect={false}
+          disabled={true}
+      />
+      <Text style={styles.label}>
+        Color:
+      </Text>
+      <Input 
+          
+          inputStyle={styles.input}
+          placeholder=''
+          value={state.data.color}
+          autoCapitalize="none"
+          autoCorrect={false}
+          disabled={true}
+      />
+      {state.data.lost  ? 
+          <Button
+          buttonStyle={styles.buttonLost}
+          title="Cambiar estado a Encontrado"
+          onPress={() =>{
+          }}
+        />
+      : 
+      <Button
+      buttonStyle={styles.buttonNotLost}
+      title="Cambiar estado a Perdido"
+      onPress={() =>{
+        
+      }}
+    />
+      }
 
       {/* {state.loading ?
         <>
@@ -76,7 +145,7 @@ const ItemRegister = ({navigation}) => {
         </>
       : null} */}
       <Spacer />
-      </View>
+      </KeyboardAwareScrollView>
     );
 }
 
@@ -85,7 +154,7 @@ const styles = StyleSheet.create({
       color: "gray"
   },
   label: {
-    marginTop: 10,
+    marginTop: 5,
     color: "black",
     marginHorizontal: 16
   },
@@ -103,13 +172,32 @@ const styles = StyleSheet.create({
   },
   input: {
     marginHorizontal: 16,
+    marginBottom: -2,
   },
-  buttonItems:  {
+  lostInput: {
+    marginHorizontal: 16,
+    marginBottom: -2,
+    color: "red"
+  },
+  notLostInput: {
+    marginHorizontal: 16,
+    marginBottom: -2,
+    color: "green"
+  },
+  buttonLost:  {
+    marginTop: 5,
     marginLeft: 5,
     marginRight: 5,
     borderRadius: 10,
-    backgroundColor: 'black'
-   }
+    backgroundColor: 'green'
+   },
+  buttonNotLost:  {
+  marginTop: 5,
+  marginLeft: 5,
+  marginRight: 5,
+  borderRadius: 10,
+  backgroundColor: 'red'
+  }
 });
 
 export default ItemRegister;
